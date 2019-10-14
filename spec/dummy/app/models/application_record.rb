9b628ccb6f2aa92329e0316cad9b0722e1d9198e
Mikhail Varabyou <varaby_m@modulotech.fr>
2019-10-14 07:04:05 +0300

    fix admin model name.

--- spec/dummy/app/models/application_record.rb
+++ spec/dummy/app/models/application_record.rb
@@ -10,7 +10,7 @@ class ApplicationRecord < ActiveRecord::Base
   # @return [String] model path for rails_admin
   def self.admin_model_name
     # uses underscore of the class name and replaces slashes "/" with tilde "~"
-    self.class.name.underscore.gsub('/', '~')
+    name.underscore.gsub('/', '~')
   end
 
   # allow to call :admin_model_name from instance
